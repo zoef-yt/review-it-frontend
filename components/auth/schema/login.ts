806d61586a3e035e7fea609e198c41676cc2ed56
Zoef <shaikhzoef36@gmail.com>
2024-09-04 23:14:50 +0530

    chore: Update login schema and navigation bar

--- components/auth/schema/login.ts
+++ components/auth/schema/login.ts
@@ -1,18 +1,6 @@
 import * as z from 'zod';
 
 export const LoginSchema = z.object({
-	usernameOrEmail: z
-		.string()
-		.min(1, { message: 'Username or Email is required' })
-		.refine(
-			(value) => {
-				const isUsername = /^[a-zA-Z0-9_]{3,}$/.test(value);
-				const isEmail = !isUsername && /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
-				return isEmail || isUsername;
-			},
-			{
-				message: 'Invalid Username or Email',
-			},
-		),
+	usernameOrEmail: z.string().min(1, { message: 'Username or Email is required' }),
 	password: z.string().min(8, { message: 'Password must be at least 8 characters long' }),
 });
--- components/home/navigationBar.tsx
+++ components/home/navigationBar.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import { useAuth } from '@/context/AuthContext';
 import Link from 'next/link';
+
+import { useAuth } from '@/context/AuthContext';
 import { Logout } from '../auth/logout/button';
 
 export function NavigationBarHome() {
@@ -25,11 +26,6 @@ export function NavigationBarHome() {
 						</>
 					) : (
 						<>
-							<Link href='/login'>
-								<span className='border border-blue-500 px-4 py-2 rounded hover:bg-blue-500 transition-colors duration-200'>
-									Login
-								</span>
-							</Link>
 							<Link href='/signup'>
 								<span className='border border-blue-500 px-4 py-2 rounded hover:bg-blue-500 transition-colors duration-200'>
 									Signup

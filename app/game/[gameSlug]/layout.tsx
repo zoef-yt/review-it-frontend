cb0f2d7ab1f894565a55c7dc085f1c5f84d9139b
Zoef <shaikhzoef36@gmail.com>
2024-09-05 13:26:59 +0530

    Refactor game review page, add loading component, and update layout

--- app/game/[gameSlug]/layout.tsx
+++ app/game/[gameSlug]/layout.tsx
@@ -1,8 +1,14 @@
 interface LayoutProps {
 	aboutGame: React.ReactNode;
+	gameReviews: React.ReactNode;
 }
 
 export default function Layout(props: LayoutProps) {
-	const { aboutGame } = props;
-	return <main className='mx-auto px-4 py-6'>{aboutGame}</main>;
+	const { aboutGame, gameReviews } = props;
+	return (
+		<main className='flex flex-col mx-auto px-4 py-6'>
+			{aboutGame}
+			{gameReviews}
+		</main>
+	);
 }

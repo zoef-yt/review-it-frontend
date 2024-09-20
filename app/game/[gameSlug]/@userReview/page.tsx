60357395d72b870bd40780190704f8fd9d1e9291
Zoef <shaikhzoef36@gmail.com>
2024-09-20 23:24:09 +0530

    fix potential issue of review component

--- app/game/[gameSlug]/@userReview/page.tsx
+++ app/game/[gameSlug]/@userReview/page.tsx
@@ -1,26 +1,24 @@
 import { makeRequest } from '@/actions/makeRequest';
 import { Game } from '@/types/game';
 import { ReviewComponent } from '@/components/singleGame/reviewComponent';
+import type { Review } from '@/types/gameReviews';
 
 export default async function UserReview({ params }: { params: { gameSlug: string } }) {
-	const [gameResponse, reviewResponse] = await Promise.all([
-		makeRequest<Game>({
-			method: 'get',
-			endpoint: `api/games/${params.gameSlug}`,
-			auth: 'none',
-		}),
-		makeRequest<any>({
-			method: 'get',
-			endpoint: `games-review/game/${params.gameSlug}`,
-			auth: 'bearer',
-		}),
-	]);
-
+	const gameResponse = await makeRequest<Game>({
+		method: 'get',
+		endpoint: `api/games/${params.gameSlug}`,
+		auth: 'none',
+	});
 	if (!gameResponse.success) {
 		return null;
 	}
 
 	const game = gameResponse.data;
+	const reviewResponse = await makeRequest<Review>({
+		method: 'get',
+		endpoint: `games-review/game/${params.gameSlug}`,
+		auth: 'bearer',
+	});
 
 	return (
 		<div className='w-full'>
